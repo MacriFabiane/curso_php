df1274f00cb625e45c23d2feae8856ff6c8ac54b
MacriFabiane <mariacristinafabiane@gmail.com>
2024-02-07 22:52:34 -0300

    !=

---
+++ 5_expressoes_operadores/5_operadoresComparacao/index.php
@@ -0,0 +1,44 @@
+<?php
+//tudo aqui serve pra variaveis tbm
+
+    //OP IGUALDADE ==
+
+
+    if(3 == "3"){//true
+        echo "comparação verdadeira 1 <br>";
+    }
+
+    if(3 == 3){//true
+        echo "comparação verdadeira 1<br>";
+    }
+
+    //OP IDENTICO A ===
+
+    
+    if(3 === "3"){//false
+        echo "comparação verdadeira 2 <br>";
+    }
+    
+    if(3 === 3){//true
+        echo "comparação verdadeira 2 <br>";
+    }
+
+    //OP DIFERENÇA !=
+
+
+    if(3 != "3"){//false
+        echo "comparação diferente 1 <br>";
+    }
+
+    if(3 != 3){//false
+        echo "comparação diferente 1<br>";
+    }
+
+    if(false != "3"){//true
+        echo "comparação diferente 2 <br>";
+    }
+
+    if(7 != 3){//true
+        echo "comparação diferente 3 <br>";
+    }
+?>
