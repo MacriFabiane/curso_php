cfb3d19b942df310a0dae98984938cbd9fcf9cee
MacriFabiane <mariacristinafabiane@gmail.com>
2024-02-09 17:05:38 -0300

    while

---
+++ 7_estruturas_de_repetição/1_while/index.php
@@ -0,0 +1,23 @@
+<?php
+
+
+$arr = [5, "Matheus", true, false, "Opa", 12.8, "Teste", true, [], "Palavra", 5, 10, "Alô"];
+
+$x = count($arr);
+$y = 0;
+
+while($y < $x) {
+
+  if(is_string($arr[$y])) {
+    echo $arr[$y] . "<br>";
+  }
+  if($y === 6){
+    echo "Terminando o loop <br>";
+    break;
+  }
+
+  $y++;
+
+}
+
+?>
