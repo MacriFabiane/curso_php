22474195a765445126d73e8804191170d1bc4348
MacriFabiane <mariacristinafabiane@gmail.com>
2024-02-09 19:26:31 -0300

    continue

--- 7_estruturas_de_repetição/1_while/index.php
+++ 7_estruturas_de_repetição/1_while/index.php
@@ -13,11 +13,37 @@ while($y < $x) {
   }
   if($y === 6){
     echo "Terminando o loop <br>";
-    break;
+    break; //para o loop
   }
 
   $y++;
 
+}
+
+// CONTINUE pula uma execução do loop
+
+$a=10;
+
+while($a > 0){
+
+    
+    if ($a ==5 || $a ==7){
+
+        echo "Pulou a execução $a <br>";
+
+        $a--;//sempre diminuir pra n ficar em infinito
+
+        continue;
+    }
+
+    if($a == 2){
+        echo "Terminando o loop break $a <br>";
+        break;
+    }
+    
+    echo "Exectando loop $a <br>";
+    
+    $a--;
 }
 
 ?>
