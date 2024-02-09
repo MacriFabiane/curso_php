c0618fa5f8a2981ae1f3f8af4c10ec03ff21f8ea
MacriFabiane <mariacristinafabiane@gmail.com>
2024-02-09 20:02:16 -0300

    foreach

---
+++ 7_estruturas_de_repetição/3_foreach/index.php
@@ -0,0 +1,9 @@
+<?php
+
+$nomes = ["Mateu", "Joao", "Pedro", "Maria"];
+
+foreach ($nomes as $nome){ //estrutura de repetição orientada a um array
+    echo "O nome do índice atual é $nome <br>";
+}
+
+?>
