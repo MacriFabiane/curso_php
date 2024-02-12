5a69a73011e6081903b4b884ecb9c1627368cf25
MacriFabiane <mariacristinafabiane@gmail.com>
2024-02-12 14:10:47 -0300

    short tags

---
+++ 8_inclusao_de_codigo/4_short_tags/index.php
@@ -0,0 +1,10 @@
+<?php
+
+    echo "Testanco codigo php nas tags normais <br>";
+?>
+
+<?//não é recomendado
+
+    echo "testando código nas short tags <br>"; //depois de mudar no servidor apache funcionou
+
+?>
