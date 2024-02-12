b3b5bdcfffa8cb63ee71ba1fe648e0307eaa5f51
MacriFabiane <mariacristinafabiane@gmail.com>
2024-02-12 13:27:13 -0300

    require

---
+++ 8_inclusao_de_codigo/2_require/arquivos/funcao.php
@@ -0,0 +1,9 @@
+<?php
+
+$idade=12;
+
+echo "Minha idade é $idade <br>";
+
+include __DIR__ . "/../testando.php";
+
+?>
---
+++ 8_inclusao_de_codigo/2_require/index.php
@@ -0,0 +1,15 @@
+<p>Testando require</p>
+
+<?php
+
+    require "teste.php";//se der erro fatal o resto do arquivo não continua a ser executado
+
+?>
+
+<p>Arquivo do include</p> 
+
+<?php
+
+    include "arquivos/funcao.php";
+    
+?>
