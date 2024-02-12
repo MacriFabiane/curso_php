f277668562f12d947c287c668a05d0ff77dfb17e
MacriFabiane <mariacristinafabiane@gmail.com>
2024-02-12 20:51:33 -0300

    print_r e var_dump

---
+++ 9_funcoes/9_depuração_de _valores/index.php
@@ -0,0 +1,19 @@
+<?php
+
+    //var_dump e print_r
+
+    $arr=[
+        "teste",
+        123,
+        123.452,
+        true,
+        [1,2,3]
+    ];
+
+    print_r($arr);
+
+    echo "<br>";
+
+    var_dump($arr);//pra humano ler fica mais bonitinho tem os tipos dos valores internos
+
+?>
