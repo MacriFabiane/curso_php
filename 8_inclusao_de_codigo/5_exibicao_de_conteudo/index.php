83ca87d0bcf5d1a20fb5f1129905657ef220a0d0
MacriFabiane <mariacristinafabiane@gmail.com>
2024-02-12 14:28:49 -0300

    exibição de conteudo

---
+++ 8_inclusao_de_codigo/5_exibicao_de_conteudo/index.php
@@ -0,0 +1,16 @@
+<?php
+    $nome = "Matheus";
+    $sobrenome = "Batisti"
+?>
+
+<form action="">
+    <div>
+        <input type="text" value="<?= $nome; ?>">
+    </div>
+    <div>
+        <input type="text" value="<?= $sobrenome; ?>">
+    </div>
+    <div>
+        <input type="submit" value="Enviar">
+    </div>
+</form>
