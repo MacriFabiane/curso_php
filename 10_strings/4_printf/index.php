05d6c626b2192091154b93dea67909a1a16e3aba
MacriFabiane <mariacristinafabiane@gmail.com>
2024-02-13 14:38:55 -0300

    printf

---
+++ 10_strings/4_printf/index.php
@@ -0,0 +1,14 @@
+<?php
+
+    //diferença do print com o printf é q o printf pode imprimir valores de forma dinamica usando %
+
+    $nome = "Macri";
+    $num=10;
+
+    printf ("O nome é %s <br>", $nome); //igual no C
+    printf ("O numero é %d e o outro %d <br>", $num, 150);
+
+    $float = 18.89098;
+
+    printf("O float é %.2f <br>", $float);
+?>
