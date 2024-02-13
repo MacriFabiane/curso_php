35f0032b236ef8f1d61560ef8c412624ded20921
MacriFabiane <mariacristinafabiane@gmail.com>
2024-02-13 13:50:09 -0300

    interpolação

---
+++ 10_strings/1_interpolacao/index.php
@@ -0,0 +1,10 @@
+<?php
+
+//o ideal é se manter com uma sintaxe só
+$nome = "Macri";
+$idade =19;
+$graduacao= "Eng. Cp";
+
+echo "Eu sou a $nome tenho $idade anos, e faço $graduacao<br>";//interpolação da variavel idade transformada em texto
+
+echo "Eu sou a {$nome} tenho {$idade} anos, e faço {$graduacao} <br>";
