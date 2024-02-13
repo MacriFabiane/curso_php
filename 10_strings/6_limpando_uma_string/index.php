920ea7022f85188f759958ec0dc0692f52616f31
MacriFabiane <mariacristinafabiane@gmail.com>
2024-02-13 16:22:55 -0300

    limpando string

---
+++ 10_strings/6_limpando_uma_string/index.php
@@ -0,0 +1,14 @@
+<?php
+
+    $str1 = "    Matheu      ";
+
+    echo "Esta é a string 1: $str1. <br>";
+
+    $str1Limpa = trim($str1);
+
+    echo "Esta é a string 1: $str1Limpa. <br>";
+
+    $str2Limpa = rtrim($str1);
+
+    echo "Esta é a string 1: $str2Limpa. <br>";
+?>
