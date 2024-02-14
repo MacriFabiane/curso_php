e80691a32d66d17928c8fedc57ae472f3518f78f
MacriFabiane <mariacristinafabiane@gmail.com>
2024-02-14 15:48:29 -0300

    range

---
+++ 11_arrays/2_range/index.php
@@ -0,0 +1,15 @@
+<?php
+
+    //range -> função utilizada para criar arrays de forma rápida
+
+    $arr = range(1, 10); //elemento inicial e elemento final
+
+    print_r($arr);
+    echo "<br>";
+
+    $arr2 = range(1, 1000, 100); // cria de 1 a 1000 mas de 100 em 100(step)
+
+    print_r($arr2);
+    echo "<br>";
+
+?>
