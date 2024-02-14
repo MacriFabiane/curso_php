897f879703a52a8731256af8abe51e5201f104db
MacriFabiane <mariacristinafabiane@gmail.com>
2024-02-14 15:48:16 -0300

    add valor no fim do array

---
+++ 11_arrays/1_add_valor_fim_do_array/index.php
@@ -0,0 +1,19 @@
+<?php
+
+    $arr=[1,2,3];
+
+    $arr[]=4;//adiciona no fim do array
+
+    print_r($arr);
+
+    echo "<br>";
+
+    $arr2=[];
+
+    $arr2[]=1;
+
+    print_r($arr2);
+
+    echo "<br>";
+
+?>
