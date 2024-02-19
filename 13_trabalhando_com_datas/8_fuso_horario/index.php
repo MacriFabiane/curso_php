8b1fd4558bf812a40eb8e806d6275cc5f9b05670
MacriFabiane <mariacristinafabiane@gmail.com>
2024-02-19 19:51:16 -0300

    timezone

--- 13_trabalhando_com_datas/8_fuso_horario/index.php
+++ 13_trabalhando_com_datas/8_fuso_horario/index.php
@@ -1,7 +1,8 @@
 <?php
 
     date_default_timezone_set('America/Sao_Paulo');//define nova timezone (antes era Berlin)
-    
+    //mais indicado calcular a diferença sabendo a timezone prédefinida
+    //usar o date_default_timezone_set é uma medida mais brusca
     $data =new DateTime();
 
     print_r($data);
