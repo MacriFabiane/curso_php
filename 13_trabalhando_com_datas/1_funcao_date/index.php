ae3e12e3150c6708d1f7849c78b337179b2d30d9
MacriFabiane <mariacristinafabiane@gmail.com>
2024-02-19 19:25:22 -0300

    date

---
+++ 13_trabalhando_com_datas/1_funcao_date/index.php
@@ -0,0 +1,19 @@
+<?php
+    $d= date('d/m/y'); //day /month/year
+
+    echo $d. "<br>";//19/02/24
+
+    $d2 = date('D, M - Y');//Mon/Feb/2024 Mon,Feb - 2024
+
+    echo $d2. "<br>";
+
+    $d3 = date('d/m/Y');//19/02/2024
+
+    echo $d3 . "<br>";
+
+    $d4 = date('l, F-Y');//Monday, February-2024
+
+    echo $d4 . "<br>";
+
+    //muitos outros formatos existentes, quando precisar ir na documentação php
+?>
