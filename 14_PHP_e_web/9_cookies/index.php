ff787feb48b95e1f48fa789ad93290c0b3c3e7f8
MacriFabiane <mariacristinafabiane@gmail.com>
2024-02-20 21:28:04 -0300

    cookies

---
+++ 14_PHP_e_web/9_cookies/index.php
@@ -0,0 +1,23 @@
+<?php
+    setcookie("nome", "Matheus", time() + 3600);//nome valor e data de expiração em tempo atual + tantos segundos
+
+    if(isset($_COOKIE['nome'])){
+        $nome = $_COOKIE['nome'];
+    }
+
+    print_r($_COOKIE)
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Olá, Mundo!</h1>
+    <?php if($nome != ""): ?>
+        <p>Seja bem-vindo <?= $nome ?></p>
+    <?php endif; ?>
+</body>
+</html>
---
+++ 14_PHP_e_web/9_cookies/pagina.php
@@ -0,0 +1,22 @@
+<?php
+    //utilizando cookie salvo no index
+    if(isset($_COOKIE['nome'])){
+        $nome = $_COOKIE['nome'];
+    }
+
+    print_r($_COOKIE)
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina</title>
+</head>
+<body>
+    <h1>Página 2</h1>
+    <?php if($nome != ""): ?>
+        <p>Seja bem-vindo <?= $nome ?></p>
+    <?php endif; ?>
+</body>
+</html>
