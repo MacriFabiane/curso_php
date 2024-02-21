216edf8c18b6b2d030fde497de942990054317b6
MacriFabiane <mariacristinafabiane@gmail.com>
2024-02-20 21:25:38 -0300

    session

---
+++ 14_PHP_e_web/10_session/index.php
@@ -0,0 +1,16 @@
+<?php
+    session_start();//inicializando seção
+   // print_r($_SESSION);
+   $_SESSION['nome']="Matheus";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Olá, session</h1>
+</body>
+</html>
